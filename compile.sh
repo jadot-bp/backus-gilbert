85796383fada70624bc02a81e8eb933220d8f640
jadot <benjaminpage.acer@gmail.com>
2022-02-02 15:14:03 +0000

    Updated compile.sh to use new covgen, covgend filenames

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ bash zcompile.sh
 # Compile scripts
 gcc "bgv5.c" -Wall -L. -linterface -lzcall -lstdc++ -lzkcm -lmpfr -lgmp -lgmpxx -o backus_v5 -fopenmp -lm -std=gnu99
 gcc consol.c -o consol -lm
-gcc bootstrap.c -o boots -lm
-gcc bootstrapv2.c -o boots_v2 -lm
+gcc covgen.c -o covgen -lm
+gcc covgend.c -o covgend -lm
 gcc compress.c -o compress -lm
 echo "done."
