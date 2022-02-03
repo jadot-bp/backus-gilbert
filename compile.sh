9e3d4cac510f9e1a977836498686117753381f3d
jadot <benjaminpage.acer@gmail.com>
2022-02-03 18:23:02 +0000

    Updated compile.sh to use new chread filename

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ bash zcompile.sh
 
 # Compile scripts
 gcc "bgv5.c" -Wall -L. -linterface -lzcall -lstdc++ -lzkcm -lmpfr -lgmp -lgmpxx -o backus_v5 -fopenmp -lm -std=gnu99
-gcc consol.c -o consol -lm
+gcc chread.c -o chread -lm
 gcc covgen.c -o covgen -lm
 gcc covgend.c -o covgend -lm
 gcc compress.c -o compress -lm
