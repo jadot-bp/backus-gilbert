35cdf49334b02c45a069e76b42e73d37c690fec4
Ben Page <47898321+jadot-bp@users.noreply.github.com>
2024-04-17 18:43:09 +0100

    Delete zcompile.sh (replaced by Makefile)

--- zcompile.sh
+++
@@ -1,7 +0,0 @@
-# Script for creating shared object files for the ZKCM interface
-
-# Compile ZKCM call shared object file
-g++ zcall.cpp -lzkcm -lmpfr -lgmp -lgmpxx -o libzcall.so -fPIC -shared
-
-# Compile C to C++ interface shared object file
-g++ interface.cpp -o libinterface.so -L. -lzcall -fPIC -shared
